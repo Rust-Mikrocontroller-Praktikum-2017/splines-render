60e2c30502a5479b90b67371013f7fc377552a77
Pfeil <andreas.pfeil@student.kit.edu>
2017-04-11 09:52:09 +0200

    Render splines with renderer lib.
    This includes using the renderers data structures and changes in the
    example in main.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -3,75 +3,20 @@
 #![no_std]
 extern crate alloc;
 extern crate collections;
+extern crate renderer;
 use collections::vec::Vec;
 use core::ops::{Add, Sub, AddAssign, SubAssign, Mul};
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Point {
-    pub x: f32,
-    pub y: f32,
-}
-
-impl Add for Point {
-    type Output = Point;
-
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-
-impl AddAssign for Point {
-    fn add_assign(&mut self, other: Point) {
-        *self = Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        };
-    }
-}
-
-impl Sub for Point {
-    type Output = Point;
-
-    fn sub(self, other: Point) -> Point {
-        Point {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        }
-    }
-}
-
-impl SubAssign for Point {
-    fn sub_assign(&mut self, other: Point) {
-        *self = Point {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        };
-    }
-}
-
-impl Mul<f32> for Point {
-    type Output = Point;
-
-    fn mul(self, other: f32) -> Point {
-        Point {
-            x: self.x * other,
-            y: self.y * other,
-        }
-    }
-}
+use renderer::coordinates::{Pixel16, Coord2D};
 
 #[derive(Debug, PartialEq)]
 pub struct ControlPoly {
-    pub data: Vec<Point>,
+    pub data: Vec<Coord2D>,
     pub closed: bool,
     pub line_thickness: u8,
 }
 
 impl ControlPoly {
-    pub fn new(points: Vec<Point>, thickness: u8, closed: bool) -> ControlPoly {
+    pub fn new(points: Vec<Coord2D>, thickness: u8, closed: bool) -> ControlPoly {
         ControlPoly {
             data: points,
             closed: closed,
@@ -80,13 +25,13 @@ impl ControlPoly {
     }
 
     pub fn eval_with_casteljau(&self) -> ControlPoly {
-        let mut output: Vec<Point> = Vec::new();
+        let mut output: Vec<Coord2D> = Vec::new();
         let mut vec = self.data.clone();
         let n = vec.len();
         output.push(vec[0]);
         output.push(vec[n - 1]);
         for k in 1..n {
-            let mut result: Vec<Point> = Vec::new();
+            let mut result: Vec<Coord2D> = Vec::new();
             for i in 0..n - k {
                 let p = ControlPoly::casteljau_divisor_half(vec[i], vec[i + 1]);
                 result.push(p);
@@ -106,16 +51,16 @@ impl ControlPoly {
         }
     }
 
-    fn casteljau_divisor(left: Point, right: Point, factor: f32) -> Point {
+    fn casteljau_divisor(left: Coord2D, right: Coord2D, factor: f32) -> Coord2D {
         debug_assert!(factor < 1_f32 && factor > 0_f32);
         left + (right - left) * factor
     }
 
-    fn casteljau_divisor_half(left: Point, right: Point) -> Point {
+    fn casteljau_divisor_half(left: Coord2D, right: Coord2D) -> Coord2D {
         left + (right - left) * 0.5_f32
     }
 
-    fn a_frame_divisor(left: Point, right: Point) -> Point {
+    fn a_frame_divisor(left: Coord2D, right: Coord2D) -> Coord2D {
         left + (right - left) * (1_f32 / 3_f32)
     }
 }
@@ -126,20 +71,20 @@ fn test_casteljau() {
     //! assuming one casteljau iteration of the given numbers.
     use collections::vec;
     let ctrl: ControlPoly = ControlPoly {
-        data: vec![Point { x: 0f32, y: 0f32 },
-                   Point { x: 0f32, y: 1f32 },
-                   Point { x: 1f32, y: 1f32 },
-                   Point { x: 1f32, y: 0f32 }],
+        data: vec![Coord2D { x: 0f32, y: 0f32 },
+                   Coord2D { x: 0f32, y: 1f32 },
+                   Coord2D { x: 1f32, y: 1f32 },
+                   Coord2D { x: 1f32, y: 0f32 }],
         closed: false, // not yet used
         line_thickness: 10, // not yet used
     };
     let draw = ctrl.eval_with_casteljau();
     let correct_draw = ControlPoly {
-        data: vec![Point { x: 0f32, y: 0f32 },
-                   Point { x: 0f32, y: 0.5 },
-                   Point { x: 0.25, y: 0.75 },
-                   Point { x: 0.5, y: 0.75 },
-                   Point { x: 1f32, y: 0f32 }],
+        data: vec![Coord2D { x: 0f32, y: 0f32 },
+                   Coord2D { x: 0f32, y: 0.5 },
+                   Coord2D { x: 0.25, y: 0.75 },
+                   Coord2D { x: 0.5, y: 0.75 },
+                   Coord2D { x: 1f32, y: 0f32 }],
         closed: false,
         line_thickness: 10,
     };
@@ -148,23 +93,23 @@ fn test_casteljau() {
 
 #[test]
 fn test_divisor_half() {
-    let p00 = Point { x: 0f32, y: 0f32 };
-    let p01 = Point { x: 0f32, y: 1f32 };
-    let p10 = Point { x: 1f32, y: 1f32 };
-    let p11 = Point { x: 1f32, y: 0f32 };
+    let p00 = Coord2D { x: 0f32, y: 0f32 };
+    let p01 = Coord2D { x: 0f32, y: 1f32 };
+    let p10 = Coord2D { x: 1f32, y: 1f32 };
+    let p11 = Coord2D { x: 1f32, y: 0f32 };
 
-    let p00_p01 = Point { x: 0f32, y: 0.5f32 };
-    let p01_p10 = Point { x: 0.5f32, y: 1f32 };
-    let p10_p11 = Point { x: 1f32, y: 0.5f32 };
-    let mid1 = Point {
+    let p00_p01 = Coord2D { x: 0f32, y: 0.5f32 };
+    let p01_p10 = Coord2D { x: 0.5f32, y: 1f32 };
+    let p10_p11 = Coord2D { x: 1f32, y: 0.5f32 };
+    let mid1 = Coord2D {
         x: 0.25f32,
         y: 0.75f32,
     };
-    let mid3 = Point {
+    let mid3 = Coord2D {
         x: 0.75f32,
         y: 0.75f32,
     };
-    let mid2 = Point {
+    let mid2 = Coord2D {
         x: 0.5f32,
         y: 0.75f32,
     };
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 extern crate splines;
-use splines::{Point, ControlPoly};
+extern crate renderer;
+use splines::ControlPoly;
+use renderer::{AABB, Renderer};
+use renderer::coordinates::{Coord2D, Pixel16};
 
 /// Minimal usage example and playground.
 fn main() {
+    // control polygon
     let ctrl: ControlPoly = ControlPoly {
-        data: vec![Point { x: 0f32, y: 0f32 },
-                   Point { x: 0f32, y: 1f32 },
-                   Point { x: 1f32, y: 1f32 },
-                   Point { x: 1f32, y: 0f32 }],
+        data: vec![Coord2D { x: 0f32, y: 0f32 },
+                   Coord2D { x: 0f32, y: 1f32 },
+                   Coord2D { x: 1f32, y: 1f32 },
+                   Coord2D { x: 1f32, y: 0f32 }],
         closed: false, // not yet used
         line_thickness: 10, // not yet used
     };
+    // ControlPoly -> spline approximation
     let draw = ctrl.eval_with_casteljau();
     println!("ctrl: {:?}\ndraw: {:?}", ctrl.data, draw.data);
+    // set up renderer
+    let func = |x, y, color| println!("({}, {})", x, y);
+    let mut r = Renderer { pixel_col_fn: func };
+    // set up drawing area (AABB)
+    let mut aabb = AABB {
+        min: Pixel16 { x: 10, y: 10 },
+        max: Pixel16 { x: 15, y: 20 },
+    };
+    // fit data into drawing area
+    let data = aabb.fit_in_aabb(draw.data.as_slice());
+    println!("{:?}", draw.data.as_slice());
+    println!("{:?}", data);
+    // plot data into given AABB.
+    r.draw_lines(aabb, data);
 }
